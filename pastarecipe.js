38aef73a6c37007d0a16f15f7775efef4b8db1d7
karapal <106742331+karapal@users.noreply.github.com>
2022-11-28 18:28:32 +0100

    .

---
+++ pastarecipe.js
@@ -0,0 +1,66 @@
+console.log ("Spaghetti alla Carbonara Recipe:");
+console.log ("\nIngredients for 4 persons:");
+
+let spaghetti = "400g";
+let guanciale = "250g";
+let veryFreshEggYolks = "6";
+let agedGratedPecorinoRomanoCheese = "50g";
+let rawBlackPepper = "4g";
+
+console.log ("spaghetti", spaghetti);
+console.log ("guanciale", guanciale);
+console.log ("very fresh egg yolks", veryFreshEggYolks);
+console.log ("aged grated Pecorino Romano cheese", agedGratedPecorinoRomanoCheese);
+console.log ("raw black pepper", rawBlackPepper);
+
+console.log ("<\nTo make classic carbonara, first cut the guanciale into 1cm layers, then into long strips.");
+console.log ("Combine the egg yolks with the finely grated Pecorino Romano.");
+
+let eggAndCheese;
+eggAndCheese = agedGratedPecorinoRomanoCheese + veryFreshEggYolks;
+
+console.log ("Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of it to the yolks and cheese mixture, set aside the rest.");
+
+eggAndCheese += rawBlackPepper * 2;
+
+console.log ("Brown the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp, then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down.");
+console.log ("Cook the pasta with a pinch of salt in the water (guanciale is already very salty);");
+
+let water = "3500g" ;
+let salt = "0,30g" ;
+let cookedPasta;
+cookedPasta = spaghetti + water + salt ;
+
+console.log ("set aside a ladleful of the pasta water before draining it, then drain the pasta once it is cooked al dente.");
+
+console.log ("Pour 2 spoons of the reserved hot water into the part of the frying pan containing the cooled guanciale and turn on the heat:");
+
+let pastaWater = "28,3g" ;
+
+console.log ("his will create a creamy sauce with the pasta starch contained in that water.");
+
+let sauce;
+sauce = pastaWater + guanciale;
+
+
+console.log ("TURN OFF THE HEAT AND SET ASIDE for 1 minute, then transfer the pasta to the same pan and mix together.");
+
+let pastaMix 
+
+pastaMix = sauce + cookedPasta;
+
+console.log ("Add the yolk and cheese mixture, stirring rapidly. In the warm pan with the hot pasta, the eggs will cook gently and become creamy.");
+
+let pastaSauce;
+
+pastaSauce = pastaMix + eggAndCheese;
+
+console.log ("It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs.");
+
+console.log ("Add the remaining roasted black pepper and serve immediately.");
+
+let carbonara;
+
+carbonara = pastaSauce + rawBlackPepper * 2;
+
+console.log ("Carbonara dish", carbonara);
